c4dfb39d58b9feaed5167cc94a118a2e8b57e9ad
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-02-13 12:30:59 +0100

    Add promises for reusability

--- ajax/main.js
+++ ajax/main.js
@@ -10,30 +10,76 @@ function getXMLHttpRequest() {
 
 const xhr = getXMLHttpRequest();
 
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-        const json = JSON.parse(xhr.responseText);
-        console.log(json);
+function get(url) {
+    return new Promise((resolve, reject) => {
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                const json = JSON.parse(xhr.responseText);
+                console.log(json);
 
-        if (xhr.status === 200) {
-            console.log("tout est ok");
+                if (xhr.status === 200) {
+                    resolve(json);
+                }
+                else {
+                    reject(xhr);
+                }
 
-            const namesElem = document.querySelector('#names');
-            json.forEach(object => {
-                const txt = document.createTextNode(object.name);
-                const li = document.createElement('li');
+            }
+        };
 
-                li.appendChild(txt);
-                namesElem.appendChild(li);
-            });
-        }
-        else {
-            console.log('Error :', xhr.status);
+        xhr.open('GET', url, true);
+        xhr.send();
 
-        }
+    })
+}
 
-    }
-};
+get('http://localhost:5500/ajax/data.json')
+    .then(json => {
+        const namesElem = document.querySelector('#names');
+        json.forEach(object => {
+            const txt = document.createTextNode(object.name);
+            const li = document.createElement('li');
 
-xhr.open('GET', 'http://localhost:5500/ajax/data.json');
-xhr.send();
+            li.appendChild(txt);
+            namesElem.appendChild(li);
+        });
+    },
+        err => {
+            console.log(err);
+
+        });
+
+// get('http://localhost:5500/ajax/data.json')
+//     .then(json => {
+//         console.log(json);
+//     })
+
+//Ca marche, mais non réutilisable sans tout copier/coller !
+
+// xhr.onreadystatechange = () => {
+//     if (xhr.readyState === 4) {
+//         const json = JSON.parse(xhr.responseText);
+//         console.log(json);
+
+//         if (xhr.status === 200) {
+//             console.log("tout est ok");
+
+//             const namesElem = document.querySelector('#names');
+//             json.forEach(object => {
+//                 const txt = document.createTextNode(object.name);
+//                 const li = document.createElement('li');
+
+//                 li.appendChild(txt);
+//                 namesElem.appendChild(li);
+//             });
+//         }
+//         else {
+//             console.log('Error :', xhr.status);
+
+//         }
+
+//     }
+// };
+
+// xhr.open('GET', 'http://localhost:5500/ajax/data.json');
+// xhr.send();
