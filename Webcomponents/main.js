14a385796c818aeb1e7ed6f810b565b24456b220
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-02-13 10:22:21 +0100

    Add shadow DOM

--- Webcomponents/main.js
+++ Webcomponents/main.js
@@ -3,19 +3,41 @@ class AppText extends HTMLElement {
     constructor() {
         //super() appelle le constructeur de la classe parente (HTMLELEMENT)
         super();
+        this.shadow = this.attachShadow({
+            mode: 'open'
+        })
         this.inputText = null;
         this.displayer = null;
     }
 
     connectedCallback() {
-        this.innerHTML = `
-        <style></style>
+        this.shadow.innerHTML = `
+        <style>
+            h1 {
+                color: royalblue;
+            }
+            input[type="text"]{
+                border ; 1px solid #333;
+                border-radius : 4px;
+                padding : 4px;
+            }
+            .app-text {
+                margin-bottom : 10px;
+            }
+        </style>
         <div class="app-text">
             <input type="text" id="app-input-text">
             <span id="app-text-displayer"></span>
         </div>
         `;
+        this.inputText = this.shadow.querySelector('#app-input-text');
+        this.displayer = this.shadow.querySelector('#app-text-displayer');
+
+        this.inputText.addEventListener('keyup', event => {
+            this.displayer.textContent = event.target.value;
+        })
     }
 };
 
-window.customElements.define('app-text', AppText); // Mise en relation de la balise HTML et de la classe Javascript
+// Mise en relation de la balise HTML et de la classe Javascript
+window.customElements.define('app-text', AppText); 
