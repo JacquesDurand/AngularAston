f0401bae07902a37c4d6d93221be98f1fcbb1f02
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-02-13 11:52:47 +0100

    Add element insertion into document

--- ajax/main.js
+++ ajax/main.js
@@ -12,7 +12,25 @@ const xhr = getXMLHttpRequest();
 
 xhr.onreadystatechange = () => {
     if (xhr.readyState === 4) {
-        console.log(xhr.responseText);
+        const json = JSON.parse(xhr.responseText);
+        console.log(json);
+
+        if (xhr.status === 200) {
+            console.log("tout est ok");
+
+            const namesElem = document.querySelector('#names');
+            json.forEach(object => {
+                const txt = document.createTextNode(object.name);
+                const li = document.createElement('li');
+
+                li.appendChild(txt);
+                namesElem.appendChild(li);
+            });
+        }
+        else {
+            console.log('Error :', xhr.status);
+
+        }
 
     }
 };
