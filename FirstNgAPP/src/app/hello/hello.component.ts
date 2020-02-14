d86c59419f30eb47507b561bd2b867050a2d2039
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-02-14 10:15:16 +0100

    Add bindings (property, event ...)

--- FirstNgAPP/src/app/hello/hello.component.ts
+++ FirstNgAPP/src/app/hello/hello.component.ts
@@ -4,15 +4,21 @@ import { Component, OnInit } from '@angular/core';
  * selector : '[app-hello]' pour selectionner sur un attribut
  */
 @Component({
-  selector: 'app-hello', 
+  selector: 'app-hello',
   templateUrl: './hello.component.html',
   styleUrls: ['./hello.component.scss']
 })
 export class HelloComponent implements OnInit {
 
+  name = '';
+  data = { ville: 'Lille' };
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getName(): string {
+    return this.name;
   }
 
 }
