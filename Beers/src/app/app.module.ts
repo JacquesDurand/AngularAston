7ba4218188a315fc090e85ebf3a8b16495be105a
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-02-20 15:11:13 +0100

    Add dashboard

--- Beers/src/app/app.module.ts
+++ Beers/src/app/app.module.ts
@@ -12,12 +12,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SidenavComponent
+    SidenavComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
